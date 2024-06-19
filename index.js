dc77a0a1991fcc30cbe157c54fd3acb6ebd69cf6
Emmraan <emran805110@gmail.com>
2024-06-19 22:31:27 +0530

    bug fix: Error: The deployment failed: Relative import path http not prefixed with / or ./ or ../

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
 import app from "./app.js";
-import { createServer } from "http";
-const portsToTry = [3000, 4000, 8000, 8001, 8080, 5000, 5500];
-(async () => {
-  for (const port of portsToTry) {
-    const availablePort = await new Promise(resolve => {
-      const server = createServer(app)
-        .listen(port)
-        .on('listening', () => server.close(() => resolve(port)))
-        .on('error', err => resolve(err.code === 'EADDRINUSE' ? null : err));
-    });
-
-    if (typeof availablePort === 'number') {
-      app.listen(availablePort, () => console.log(`Server is running on http://localhost:${availablePort}`));
-      return;
-    } else if (availablePort instanceof Error) {
-      console.error(`Error while trying to listen on port ${port}:`, availablePort);
-      return;
-    }
-  }
-  console.error(`All ports are in use. Could not start the server. Make sure one of the following ports is available: ${portsToTry}`);
-})();
+const PORT = 3000
+app.listen(PORT,  () =>  {
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
